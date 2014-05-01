48f411277c55307a7ecfee4d6cd35a5c5344c24e
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-01 17:55:58 -0600

    refactor iterating-arrays-of-hashes

--- iterating-arrays-of-hashes/dogs.rb
+++ iterating-arrays-of-hashes/dogs.rb
@@ -1,11 +1,11 @@
 class Dogs
-  POOR      = (0..5).to_a.sample
-  AVERAGE   = (6..10).to_a.sample
+  POOR = (0..5).to_a.sample
+  AVERAGE = (6..10).to_a.sample
   EXCELLENT = (11..15).to_a.sample
 
   def initialize
-    joe   = {
-      :name => {:first => "Joe", :last=> "Smith"},
+    joe = {
+      :name => {:first => "Joe", :last => "Smith"},
       :owner_quality => EXCELLENT
     }
     sarah = {
@@ -23,76 +23,42 @@ class Dogs
              {:name => "Tank", :size => :huge, :owner => sarah},
              {:name => "Beast", :size => :large, :owner => sarah},
              {:name => "Harleigh", :size => :medium, :owner => andrew},
-             {:name => "Trixie", :size => :small, :owner => andrew},]
+             {:name => "Trixie", :size => :small, :owner => andrew},
+    ]
   end
 
   # only edit below this line
 
   def small_dogs
-    small_dogs = []
-    @dogs.cycle do |dog|
-      small_dogs << dog if dog[:size] == :small
-      break if dog[:name] == "Trixie"
-    end
-    small_dogs
+    @dogs.select { |dog| dog[:size] == :small }
   end
 
   def huge_dog
-    huge_dogs = []
-    @dogs.cycle do |dog|
-      huge_dogs << dog if dog[:size] == :huge
-      break if dog[:name] == "Trixie"
-    end
-    huge_dogs.first
+    @dogs.find { |dog| dog[:size] == :huge }
   end
 
   def large_dog_names
-    large_dogs = []
-    @dogs.cycle do |dog|
-      large_dogs << dog[:name] if dog[:size] == :large
-      break if dog[:name] == "Trixie"
-    end
-    large_dogs
+    @dogs.map { |dog| dog[:name] if dog[:size] == :large }.compact
   end
 
   def joes_large_dogs
-    joes_large_dogs = []
-    @dogs.cycle do |dog|
-      joes_large_dogs << dog[:name] if dog[:size] == :large && dog[:owner][:name][:first] == 'Joe'
-      break if dog[:name] == "Trixie"
-    end
-    joes_large_dogs
+    @dogs.map do |dog|
+      dog[:name] if dog[:size] == :large && dog[:owner][:name][:first] == 'Joe'
+    end.compact
   end
 
   def sizes
-    dog_sizes = []
-    @dogs.cycle do |dog|
-      dog_sizes << dog[:size]
-      dog_sizes.uniq!
-      break if dog[:name] == "Trixie"
-    end
-    dog_sizes
+    @dogs.map{ |dog| dog[:size] }.uniq
   end
 
   def owners
-    dog_owners = []
-    @dogs.cycle do |dog|
-      owner = "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"
-      dog_owners << owner
-      dog_owners.uniq!
-      break if dog[:name] == "Trixie"
-    end
-    dog_owners
+    @dogs.map{ |dog| "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"}.uniq
   end
 
   def average_owners
-    dog_owners = []
-    @dogs.cycle do |dog|
-      owner = "#{dog[:owner][:name][:first]} #{dog[:owner][:name][:last]}"
-      dog_owners << owner if dog[:owner][:owner_quality] == AVERAGE
-      dog_owners.uniq!
-      break if dog[:name] == "Trixie"
+    @dogs.select! do |dog|
+      dog[:owner][:owner_quality] == AVERAGE
     end
-    dog_owners
+    self.owners
   end
 end

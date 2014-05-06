bc31c2b159b5c1bbb0a3b078c232f8879d45083b
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-06 15:48:38 -0600

    User can return a hash of headers from a http response string

---
+++ parsing-http/lib/http_response.rb
@@ -0,0 +1,21 @@
+class HttpResponse
+  def initialize(http_response)
+    @response = http_response
+  end
+
+  def headers
+    pre_headers = @response.lines
+    pre_headers.shift if pre_headers[0] == "\n"
+    headers = {}
+    pre_headers.each do |pre_header|
+      break if pre_header == "\n"
+      pre_header.lstrip!.chomp!
+      header_combo = pre_header.split(': ')
+      if header_combo.length == 2
+        headers[header_combo[0]] = header_combo[1]
+      end
+    end
+    headers
+  end
+
+end
---
+++ parsing-http/spec/http_response_spec.rb
@@ -0,0 +1,40 @@
+require_relative '../lib/http_response'
+require 'rspec'
+
+describe 'HttpResponse' do
+
+  let(:response_text) do
+    %Q{
+    HTTP/1.1 200 OK
+    Server: nginx/1.4.6 (Ubuntu)
+    Date: Tue, 06 May 2014 02:17:16 GMT
+    Content-Type: text/html
+    Last-Modified: Sun, 27 Apr 2014 04:03:41 GMT
+    Transfer-Encoding: chunked
+    Connection: keep-alive
+    Content-Encoding: gzip
+
+    <!DOCTYPE html>
+              <html lang="en">
+    <head><meta charset="utf-8" />
+    <meta name="description" content="should i test private methods?" />
+    <meta name="keywords" content="test,private,methods,oo,object,oriented,tdd" />
+    <title>Should I Test Private Methods?</title>
+    </head>
+    <body>
+    <div style='font-size: 96px; font-weight: bold; text-align: center; padding-top: 200px; font-family: Verdana, Helvetica, sans-serif'>NO</div>
+      <!-- Every time you consider testing a private method, your code is telling you that you haven't allocated responsibilities well.  Are you listening to it? -->
+    </body>
+    </html>
+    }
+  end
+
+  it 'returns a hash of headers for header method' do
+    http_response = HttpResponse.new(response_text)
+    expect(response_text.class).to eq(String)
+    headers = http_response.headers
+    expect(headers.class).to eq(Hash)
+    expect(headers.keys.first.class).to eq(String)
+  end
+
+end

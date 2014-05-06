495f0119244ef9cf60d7ea43aad57d63e56a9cf0
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-06 16:00:13 -0600

    Changed header test expectation to match actual output for hardcoded http response string

--- parsing-http/spec/http_response_spec.rb
+++ parsing-http/spec/http_response_spec.rb
@@ -32,9 +32,9 @@ describe 'HttpResponse' do
   it 'returns a hash of headers for header method' do
     http_response = HttpResponse.new(response_text)
     expect(response_text.class).to eq(String)
-    headers = http_response.headers
-    expect(headers.class).to eq(Hash)
-    expect(headers.keys.first.class).to eq(String)
+    actual = http_response.headers
+    expected = {"Server"=>"nginx/1.4.6 (Ubuntu)", "Date"=>"Tue, 06 May 2014 02:17:16 GMT", "Content-Type"=>"text/html", "Last-Modified"=>"Sun, 27 Apr 2014 04:03:41 GMT", "Transfer-Encoding"=>"chunked", "Connection"=>"keep-alive", "Content-Encoding"=>"gzip"}
+    expect(actual).to eq(expected)
   end
 
 end

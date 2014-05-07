1d8c1d92eaf6a81121d3abc60d9f84a971143c73
EmilyPlatzer <emilyplatzer@gmail.com>
2014-05-07 16:00:03 -0600

    hash-1.0 - people (joe and sue)

--- hash-1.0/01_last_names.rb
+++ hash-1.0/01_last_names.rb
@@ -1,3 +1,6 @@
 require_relative 'people'
 
 # What are the last names of all of the people?
+PEOPLE.each do |person, data|
+  p data[:last_name]
+end
--- hash-1.0/02_first_genre.rb
+++ hash-1.0/02_first_genre.rb
@@ -1,3 +1,6 @@
 require_relative 'people'
 
 # What is the first genre that each person listed?
+PEOPLE.each do |person, data|
+  p data[:preferences][:favorite_genres].first
+end
